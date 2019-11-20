73b56be27973795518e195b8d6df0bb51219d742
Polar Bear <bballare21@gmail.com>
2019-11-20 13:41:46 -0800

    added event model and included call and text model

---
+++ models/event.js
@@ -0,0 +1,52 @@
+module.exports = function(sequelize, DataTypes) {
+  let Events = sequelize.define("Events", {
+    event_date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    event_time: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    event_time_meridiem: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    event_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    event_location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    event_date_picture: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      }
+    },
+    {timestamps: false
+    });
+
+    Events.associate = function(models) {
+      Events.hasOne(models.Call, {
+        onDelete: 'cascade'
+      });
+    };
+
+    Events.associate = function(models) {
+      Events.hasOne(models.Text, {
+        onDelete: 'cascade'
+      });
+    };
+  return Events;
+};
