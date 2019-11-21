41c53cfa270d02e53c83bef3c86c17395dcdc584
Marti <52050778+github-marti@users.noreply.github.com>
2019-11-21 10:59:44 -0800

    updated call and text models to belong to user

--- models/call.js
+++ models/call.js
@@ -1,24 +1,34 @@
-module.exports = function(sequelize, DataTypes) {
-    let Call = sequelize.define("Call", {
-      call_time: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }},
-        call_type: {
-            type: DataTypes.STRING,
-            allowNull: false,
+module.exports = function (sequelize, DataTypes) {
+  let Call = sequelize.define("Call", {
+    call_time: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    call_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
 
-        },
-      });
+    },
+  });
 
-      Call.associate = function(models) {
-          Call.belongsTo(models.Events, {
-              foriegnKey: {
-                  allowNull: false
-              }
-          });
-      };
-    return Call;
+  Call.associate = function (models) {
+    Call.belongsTo(models.Events, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
   };
+
+  Call.associate = function (models) {
+    Call.belongsTo(models.Events, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Call;
+};
