adea3564f1fd606d909a86a07ef17df568d9dad4
Marti <52050778+github-marti@users.noreply.github.com>
2019-11-20 21:50:30 -0800

    added calls/texts controllers to controllers index

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
 const usersController = require("usersController");
 const eventsController = require("eventsController");
 
-modules.export = usersController, eventsController;
+modules.export = usersController, eventsController, callsController, textsController;
