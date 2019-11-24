47b157e5860206a62e3fc178e782544d0503ce08
Marti <52050778+github-marti@users.noreply.github.com>
2019-11-23 16:37:42 -0800

    changed instances of dates to events for consistency

--- routes/event-api-routes.js
+++ routes/event-api-routes.js
@@ -13,11 +13,11 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage});
 
 module.exports = function (app) {
-    app.get("/api/users/:userid/dates", eventsController.getAll);
-    app.get("/api/users/:userid/dates/:id", eventsController.getOne);
-    app.get("/dates/:guid", eventsController.getByGUID);
-    app.post("/api/dates", eventsController.create);
-    app.post("/api/dates/:id/images/upload", upload.single('image'), eventsController.uploadImage);
-    app.put("/api/dates/:id", eventsController.update);
-    app.delete("/api/dates/:id", eventsController.delete);
+    app.get("/api/users/:userid/events", eventsController.getAll);
+    app.get("/api/users/:userid/events/:id", eventsController.getOne);
+    app.get("/events/:guid", eventsController.getByGUID);
+    app.post("/api/events", eventsController.create);
+    app.post("/api/events/:id/images/upload", upload.single('image'), eventsController.uploadImage);
+    app.put("/api/events/:id", eventsController.update);
+    app.delete("/api/events/:id", eventsController.delete);
 }
