eba1595016c492de91cc22548ee6e780c42b6f1d
Marti <52050778+github-marti@users.noreply.github.com>
2019-11-25 10:25:16 -0800

    public page now shows information from api call

--- client/src/components/EventPage/index.js
+++ client/src/components/EventPage/index.js
@@ -3,22 +3,35 @@ import API from '../../utils/eventAPI'
 
 class EventPage extends React.Component {
     state = {
-        eventName: "",
-        eventDate: "",
-        eventTime: "",
-        eventLocation: "",
-        eventImage: ""
+        event_name: "",
+        event_date: "",
+        event_time: "",
+        event_location: "",
+        event_date_picture: ""
     }
 
     async componentDidMount() {
         const { shortid } = this.props.match.params;
         let eventData = await API.getEventByShortId(shortid);
         console.log(eventData.data);
+        this.setState({
+            event_name: eventData.data.event_name,
+            event_date: eventData.data.event_date,
+            event_time: eventData.data.event_time,
+            event_location: eventData.data.event_location,
+            event_date_picture: eventData.data.event_date_picture
+        })
     }
 
     render() {
         return (
-            <h1>Public page</h1>
+            <div>
+                <h1>Public Event Page</h1>
+                <p>Date: {this.state.event_date}</p>
+                <p>Time: {this.state.event_time}</p>
+                <p>Location: {this.state.event_location}</p>
+                <p><img src={this.state.event_date_picture}></img></p>
+            </div>
         )
     }
 
