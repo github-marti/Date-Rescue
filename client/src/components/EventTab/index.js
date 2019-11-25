9cde32037b7b990635f25c808518fcf1d71f3cd3
Marti <52050778+github-marti@users.noreply.github.com>
2019-11-25 14:12:27 -0800

    changed nav to event tab

---
+++ client/src/components/EventTab/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function EventTab() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a className="nav-link active" href="www.google.com">Create-A-Date</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="www.google.com">View a past date</a>
+      </li>
+    </ul>
+  );
+}
+
+export default EventTab;
