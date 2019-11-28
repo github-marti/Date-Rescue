3117ad5a7b9169966b7b63169af2a1e7e3f8000d
Marti <52050778+github-marti@users.noreply.github.com>
2019-11-28 12:55:00 -0800

    upcoming events component now shows 'no upcoming events' when applicable

--- client/src/components/UpcomingEvent/index.js
+++ client/src/components/UpcomingEvent/index.js
@@ -8,15 +8,19 @@ function UpcomingEvent () {
 
     return (
         <div>
-            <EventCard 
-            event_name={state.currentEvent.event_name}
-            event_date={state.currentEvent.event_date}
-            event_time={state.currentEvent.event_time}
-            event_location={state.currentEvent.event_location}
-            event_note={state.currentEvent.event_note}
-            event_date_picture={state.currentEvent.event_date_picture}
-            formatTime={state.formatTime}
+            {state.currentEvent.id ? ( 
+                <EventCard 
+                event_name={state.currentEvent.event_name}
+                event_date={state.currentEvent.event_date}
+                event_time={state.currentEvent.event_time}
+                event_location={state.currentEvent.event_location}
+                event_note={state.currentEvent.event_note}
+                event_date_picture={state.currentEvent.event_date_picture}
+                formatTime={state.formatTime}
             />
+            ) : (
+                <h3>You don't have any upcoming events.</h3>
+            )}
         </div>
     )
 }
