380e41e026450f59299a934a6a70f1411865b632
Marti <52050778+github-marti@users.noreply.github.com>
2019-12-04 20:19:56 -0800

    changed conditional to results.data

--- client/src/components/UpcomingEvent/index.js
+++ client/src/components/UpcomingEvent/index.js
@@ -11,7 +11,8 @@ function UpcomingEvent() {
     useEffect(() => {
         API.getEvents(state.userid)
             .then(results => {
-                if (results) {
+                console.log("RESULTS UPCOMING", results.data);
+                if (results.data) {
                     let upcomingEvents = results.data.filter(el => el.active && (Date.parse(`${el.event_date}T${el.event_time}`) + 10800000) > Date.parse(new Date()));
                     console.log('show upcoming event', upcomingEvents[0])
                     dispatch({
