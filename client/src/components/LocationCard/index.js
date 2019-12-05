6df0897c44a8a0f4520e6ac15b410c2d8dd74a74
Marti <52050778+github-marti@users.noreply.github.com>
2019-12-05 00:28:06 -0800

    map now appears based on location name

--- client/src/components/LocationCard/index.js
+++ client/src/components/LocationCard/index.js
@@ -10,38 +10,46 @@ function CreateCard(props) {
     const [state] = useStoreContext();
 
     const handleLikeIncrement = event => {
-        API.updateLike({likes:state.locations[props.i].location_like, id:state.locations[props.i].id}).then(data=>{
+        API.updateLike({ likes: state.locations[props.i].location_like, id: state.locations[props.i].id }).then(data => {
             console.log(data)
             props.click()
         });
-        
+
         event.preventDefault();
     }
 
     const handleDisLikeIncrement = event => {
-        API.updateDisLike({dislikes:state.locations[props.i].location_dislike, id:state.locations[props.i].id}).then(data=>{
+        API.updateDisLike({ dislikes: state.locations[props.i].location_dislike, id: state.locations[props.i].id }).then(data => {
             props.click()
         });
-        
+
         event.preventDefault();
     }
     const thumbsUp = <FontAwesomeIcon icon={faThumbsUp} />
     const thumbsDown = <FontAwesomeIcon icon={faThumbsDown} />
-    
 
-    return(
-       
+
+    return (
+
         <div id="card">
             <h4>{props.data.location_name}</h4>
             <p>{props.data.location_address}</p>
             <p>{props.data.location_city}, {props.data.location_state}. {props.data.location_zip}</p>
+            <iframe
+                className="map-container"
+                width="300"
+                height="200"
+                frameBorder="0"
+                src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_API_KEY}&q=${props.data.location_name}`}
+                allowFullScreen>
+            </iframe>
             <p>Does this location offer an angel shot service?  {props.data.angel_shot}</p>
-            <button onClick={handleLikeIncrement} id="thumbsup">{thumbsUp} {state.locations[props.i].location_like||0}</button>
-            <button onClick={handleDisLikeIncrement} id="thumbsdown">{thumbsDown} {state.locations[props.i].location_dislike||0}</button>
+            <button onClick={handleLikeIncrement} id="thumbsup">{thumbsUp} {state.locations[props.i].location_like || 0}</button>
+            <button onClick={handleDisLikeIncrement} id="thumbsdown">{thumbsDown} {state.locations[props.i].location_dislike || 0}</button>
         </div>
     )
 }
-       
+
 
 
 export default CreateCard
