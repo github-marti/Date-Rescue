2cb3aff901d7e17f6d7fcd0efe9fa3dd86450fd5
Marti <52050778+github-marti@users.noreply.github.com>
2019-12-12 19:17:51 -0800

    testing out getting only from /

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
   const path = require('path');
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
