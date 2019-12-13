32e07e7ab67219bdadb881564530c2a929ce3a0b
Marti <52050778+github-marti@users.noreply.github.com>
2019-12-12 19:45:10 -0800

    xx

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ app.use(passport.session());
 app.use(bodyParser());
 
 // Static directory
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-//   const path = require('path');
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Check for new calls and compare them with current loaded upcoming call
 app.use(compareCalls());
